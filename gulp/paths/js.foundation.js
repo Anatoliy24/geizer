07dd0abf9be1c6d1ac276b986b286f27f19bbedf
Анатолий В. Капп <u143@stekspb.ru>
2018-04-10 18:34:50 +0300

    last commit

---
+++ gulp/paths/js.foundation.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = [
+  './node_modules/jquery/dist/jquery.min.js',
+  './node_modules/bxslider/dist/jquery.bxslider.min.js'
+];
---
+++ source/js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".slider-m-l, .slider-m-r").bxSlider({
+    controls: false,
+    auto: true
+  });
+});
